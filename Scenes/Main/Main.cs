395b5879313ac5cc168d2fcef6b52927f6dcd330
bin549 <549288160@qq.com>
2026-06-20 20:10:44 +0800

    update

--- Scenes/Main/Main.cs
+++ Scenes/Main/Main.cs
@@ -1,12 +1,43 @@
 using Godot;
 
 public partial class Main : Control {
+    private bool isStarting;
+
+    public override void _Input(InputEvent @event) {
+        if (IsStartPressed(@event)) {
+            StartGame();
+            GetViewport().SetInputAsHandled();
+            return;
+        }
+
+        if (@event.IsActionPressed("quit")) {
+            GetTree().Quit();
+            GetViewport().SetInputAsHandled();
+        }
+    }
+
     public override void _Process(double delta) {
         if (Input.IsActionJustPressed("confirm")) {
-            GameManager.LoadNextLevelScene();
+            StartGame();
         }
         if (Input.IsActionJustPressed("quit")) {
             GetTree().Quit();
         }
+    }
+
+    private bool IsStartPressed(InputEvent @event) {
+        if (@event.IsActionPressed("confirm")) return true;
+        if (@event is not InputEventKey keyEvent) return false;
+        if (!keyEvent.Pressed || keyEvent.Echo) return false;
+
+        return keyEvent.Keycode == Key.E ||
+               keyEvent.PhysicalKeycode == Key.E ||
+               keyEvent.KeyLabel == Key.E;
+    }
+
+    private void StartGame() {
+        if (this.isStarting) return;
+        this.isStarting = true;
+        GameManager.LoadNextLevelScene();
     }
 }
